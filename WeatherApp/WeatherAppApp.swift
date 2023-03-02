9dcf2accb79736cec8873dacfff47217c6e7cfee
tharmJaa <timeruchu0808@gmail.com>
2023-03-02 22:34:51 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Time Ruchutrakool on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
